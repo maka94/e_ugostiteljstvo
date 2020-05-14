cca38e870212911a83d3d19a318cd0126f472608
Marija <pesicmaria94@gmail.com>
2020-05-14 22:58:12 +0200

    added residence field

--- apps/reservations/serializers.py
+++ apps/reservations/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from apps.reservations.models import Reservation
 from apps.residences.models import Residence
+from apps.residences.serializers import ResidenceSerializer
 
 class CreateReservationSerializer(serializers.Serializer):
     date_from = serializers.DateField()
@@ -8,7 +9,8 @@ class CreateReservationSerializer(serializers.Serializer):
     residence = serializers.PrimaryKeyRelatedField(queryset=Residence.objects.all())
 
 class ReservationSerializer(serializers.ModelSerializer):
+    residence = ResidenceSerializer(read_only=True)
     class Meta:
         model = Reservation
-        fields = ['id', 'user', 'residence', 'date_from', 'date_to', 'cancelled', 'price']
+        fields = ['id', 'user', 'residence', 'date_from', 'date_to', 'cancelled', 'price', 'residence']
 
