2176bcb13e2e006d40b3ec966e420ad4647d1f7c
Marija <pesicmaria94@gmail.com>
2020-05-17 21:41:25 +0200

    model registered to admin site

--- apps/residences/admin.py
+++ apps/residences/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from apps.residences.models import Residence
+from apps.residences.models import Residence, ResidenceImage
 
 admin.site.register(Residence)
+admin.site.register(ResidenceImage)
 
